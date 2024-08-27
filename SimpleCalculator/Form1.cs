529644c30e6389d5925412da754de7ee0c3b4d11
6thgrave <esemekivwaroomamerhi@gmail.com>
2024-08-27 18:49:32 +0100

    Add files via upload

---
+++ SimpleCalculator/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SimpleCalculator
+{
+    public partial class Form1 : Form
+    {
+        private double result = 0;
+        private string operation = "";
+        private bool isOperationPerformed = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            if ((txtDisplay.Text == "0") || (isOperationPerformed))
+                txtDisplay.Clear();
+
+            isOperationPerformed = false;
+            Button button = (Button)sender;
+            if (button.Text == ".")
+            {
+                if (!txtDisplay.Text.Contains("."))
+                    txtDisplay.Text += button.Text;
+            }
+            else
+            {
+                txtDisplay.Text += button.Text;
+            }
+        }
+
+        private void operator_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            operation = button.Text;
+            result = double.Parse(txtDisplay.Text);
+            isOperationPerformed = true;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtDisplay.Text = "0";
+            result = 0;
+            operation = "";
+        }
+
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+            switch (operation)
+            {
+                case "+":
+                    txtDisplay.Text = (result + double.Parse(txtDisplay.Text)).ToString();
+                    break;
+                case "-":
+                    txtDisplay.Text = (result - double.Parse(txtDisplay.Text)).ToString();
+                    break;
+                case "*":
+                    txtDisplay.Text = (result * double.Parse(txtDisplay.Text)).ToString();
+                    break;
+                case "/":
+                    txtDisplay.Text = (result / double.Parse(txtDisplay.Text)).ToString();
+                    break;
+                default:
+                    break;
+            }
+            result = double.Parse(txtDisplay.Text);
+            operation = "";
+        }
+    }
+}
